bb15ef35aa5af4d9aa8b6a154f2c37b705a52562
André FR Silva <43449595+andrefrsilva@users.noreply.github.com>
2025-11-20 20:45:04 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'medicinup-v3-fast';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/aos@2.3.1/dist/aos.css',
+  'https://unpkg.com/aos@2.3.1/dist/aos.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js',
+  'https://visjs.github.io/vis-timeline/standalone/umd/vis-timeline-graph2d.min.js',
+  'https://visjs.github.io/vis-timeline/styles/vis-timeline-graph2d.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
